731ad0244cac9f1384ce94d962193ab49de01275
lchen6283 <lchen6283@gmail.com>
2022-08-30 11:51:03 -0400

    details page

---
+++ front-end/src/Components/PokemonDetails.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import heartSolid from "../assets/heart-solid.png";
+import heartOutline from "../assets/heart-regular.png";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+
+function PokemonDetails() {
+  const [pokemon, setPokemon] = useState([]);
+  let { id } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get(`${API}/pokemon/${id}`)
+      .then((res) => {
+        setPokemon(res.data.payload);
+        navigate(`/pokemon/${id}`);
+      })
+      .catch(() => {
+        navigate("/not found");
+      });
+  }, [id, navigate]);
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/pokemon/${id}`)
+      .then(() => {
+        navigate("/pokemon");
+      })
+      .catch(() => {
+        console.warn("error");
+      });
+  };
+  return (
+    <div className="pokemon-details">
+      <hr></hr>
+      <aside>
+        <h2>
+          {pokemon.favorite
+            ? "This pokemon is AWESOME!"
+            : "This pokemon is okay."}
+        </h2>
+        <img
+          className="hearts"
+          src={pokemon.favorite ? heartSolid : heartOutline}
+          alt={pokemon.favorite ? "AWESOME POKEMON" : "Okay Pokemon"}
+        />
+      </aside>
+      <article>
+        <h2>Pokedex No. {pokemon.pokedex}</h2>
+        <div>
+          <img className="image" src={pokemon.image} alt={pokemon.name} />
+          <h2>Name: {pokemon.name}</h2>
+          <h2>Type: {pokemon.type}</h2>
+          <h2>Region: {pokemon.region}</h2>
+        </div>
+
+        <div className="showNavigation">
+          <div>
+            <Link to={`/pokemon`}>
+              <button>Back</button>
+            </Link>
+          </div>
+          <div>
+            <Link to={`/pokemon/${pokemon.id}/edit`}>
+              <button>Edit</button>
+            </Link>
+          </div>
+          <div>
+            <button onClick={handleDelete}>Delete</button>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}
+export default PokemonDetails;
---
+++ front-end/src/Pages/Show.js
@@ -0,0 +1,14 @@
+import PokemonDetails from "../Components/PokemonDetails";
+
+function Show() {
+  return (
+    <div className="Show">
+      <header>
+        <h1>Pokemon</h1>
+      </header>
+      <PokemonDetails />
+    </div>
+  );
+}
+
+export default Show;
